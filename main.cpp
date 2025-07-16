28ec931faa8024a7870a51eed609f08fc96617f4
acproject <acproject@qq.com>
2025-07-16 14:42:16 +0800

    refactor: 修改文件权限
    - 修改 CMakeLists.txt 文件权限- 修改 main.cpp 文件权限

--- main.cpp
+++ main.cpp
@@ -20,8 +20,10 @@ typedef struct {
 } AppData;
 
 bool load_model(AppData *data, const std::string &model_path) {
+    std::cout << "[LOG] 开始加载模型: " << model_path << std::endl;
     try {
         data->module = torch::jit::load(model_path);
+        std::cout << "[LOG] 模型加载成功" << std::endl;
         return true;
     } catch (const c10::Error &e) {
         std::cerr << "Model load failed: " << e.msg() << std::endl;
@@ -105,39 +107,60 @@ static void on_window_destroy(GtkWidget *widget, gpointer user_data) {
 }
 
 static void activate(GtkApplication *app, gpointer user_data) {
+    std::cout << "[LOG] 进入 activate 回调" << std::endl;
     AppData *data = g_new(AppData, 1);
-    if (!load_model(data, "resnet18.pth")) {
-        std::cerr << "Failed to load model" << std::endl;
+    GtkWidget *window = gtk_application_window_new(app);
+    std::string model_path = "resnet18.pth";
+    GtkNativeDialog *native_dialog = (GtkNativeDialog *)gtk_file_chooser_native_new("选择模型文件", GTK_WINDOW(window), GTK_FILE_CHOOSER_ACTION_OPEN, "_打开", "_取消");
+    g_signal_connect(native_dialog, "response", G_CALLBACK(+[](GtkNativeDialog *dialog, gint response_id, gpointer user_data) {
+        std::string *model_path = static_cast<std::string *>(user_data);
+        if (response_id == GTK_RESPONSE_ACCEPT) {
+            GFile *file = gtk_file_chooser_get_file(GTK_FILE_CHOOSER(dialog));
+            if (file) {
+                char *filename = g_file_get_path(file);
+                *model_path = filename;
+                g_free(filename);
+                g_object_unref(file);
+            }
+        }
+        gtk_window_destroy(GTK_WINDOW(GTK_NATIVE_DIALOG(dialog)));
+    }), &model_path);
+    gtk_native_dialog_show(native_dialog);
+    if (!load_model(data, model_path)) {
+        GtkWidget *error_dialog = gtk_message_dialog_new(NULL, GTK_DIALOG_MODAL, GTK_MESSAGE_ERROR, GTK_BUTTONS_OK, "加载模型失败: %s", model_path.c_str());
+        gtk_native_dialog_show(GTK_NATIVE_DIALOG(error_dialog));
+    g_signal_connect(error_dialog, "response", G_CALLBACK(+[](GtkNativeDialog *dialog, gint response_id, gpointer user_data) {
+        gtk_window_destroy(GTK_WINDOW(GTK_NATIVE_DIALOG(dialog)));
+    }), NULL);
         return;
     }
-
-    GtkWidget *window = gtk_application_window_new(app);
+    GtkWidget *message_dialog = gtk_message_dialog_new(GTK_WINDOW(window), GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_OK, "模型已加载，准备创建窗口");
     gtk_window_set_title(GTK_WINDOW(window), "GTK4 示例程序");
+    gtk_native_dialog_show(GTK_NATIVE_DIALOG(message_dialog));
+    g_signal_connect(message_dialog, "response", G_CALLBACK(+[](GtkNativeDialog *dialog, gint response_id, gpointer user_data) {
+        gtk_window_destroy(GTK_WINDOW(GTK_NATIVE_DIALOG(dialog)));
+    }), NULL);
+    std::cout << "[LOG] 模型已加载，准备创建窗口" << std::endl;
     gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
-
     GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
     gtk_widget_set_margin_start(box, 20);
-    gtk_widget_set_margin_end(box, 20);
+    gtk_widget_set_margin_end(box, 20); 
     gtk_widget_set_margin_top(box, 20);
     gtk_widget_set_margin_bottom(box, 20);
     gtk_window_set_child(GTK_WINDOW(window), box);
-
     data->image_area = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
     gtk_box_append(GTK_BOX(box), data->image_area);
-
     data->result_label = gtk_label_new("等待图像");
     gtk_box_append(GTK_BOX(box), data->result_label);
-
     GtkWidget *image_button = gtk_button_new_with_label("选择图像");
     g_signal_connect(image_button, "clicked", G_CALLBACK(on_image_button_clicked), data);
     gtk_box_append(GTK_BOX(box), image_button);
-
-    
-
+    std::cout << "[LOG] 所有控件已创建" << std::endl;
     g_signal_connect(window, "destroy", G_CALLBACK(on_window_destroy), data);
-
     gtk_widget_set_visible(window, TRUE);
+    std::cout << "[LOG] 窗口已设置为可见" << std::endl;
 }
+
 #ifdef _WIN32
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     int argc = __argc;
